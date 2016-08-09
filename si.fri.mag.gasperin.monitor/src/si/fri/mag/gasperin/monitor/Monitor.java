9c1675ebbfa77a224252c24653b6d0d292fc8e01
gasperinn <gasperinn@gmail.com>
2016-08-09 12:21:36 +0200

    Add files via upload

--- si.fri.mag.gasperin.monitor/src/si/fri/mag/gasperin/monitor/Monitor.java
+++ si.fri.mag.gasperin.monitor/src/si/fri/mag/gasperin/monitor/Monitor.java
@@ -6,21 +6,27 @@ import com.sun.net.httpserver.HttpExchange;
 import com.sun.net.httpserver.HttpHandler;
 import com.sun.net.httpserver.HttpServer;
 
+import java.io.BufferedOutputStream;
 import java.io.BufferedWriter;
 import java.io.DataOutputStream;
+import java.io.FileInputStream;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.OutputStream;
 import java.io.PrintStream;
 import java.io.StringReader;
 import java.io.Writer;
 import java.net.HttpURLConnection;
 import java.net.InetSocketAddress;
+import java.net.ProtocolException;
 import java.net.URL;
+import java.net.URLEncoder;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Iterator;
 import java.util.List;
+import java.util.Properties;
 import java.util.Set;
 
 import javax.xml.parsers.DocumentBuilder;
@@ -37,22 +43,120 @@ import si.fri.mag.gasperin.gcm.GCMContent;
 
 public class Monitor
 {
-  private static int portCEP = 1400;
-  private static String context = "/monitor";
+  private static int portCEP;
+  private static String context;
+  private static boolean autoSubscribe;
   
-  final static String GCM_API_KEY = "***";
-  final static String GCM_DEVICE_ID="***";
+  private static String om2mUsername;
+  private static String om2mPassword;
+  private static String om2mIp;
+  private static String om2mPathToProduct;
+  private static String om2mDeviceName;
+  private static String om2mContainerName;
+  private static String om2mSubContainer;
+  
+  private static boolean postToGcmBool;
+  private static String GCM_API_KEY;
+  private static String GCM_DEVICE_ID;
+  private static String GCM_TITLE;
 
   public static void main(String[] args) throws Exception
   {
     System.out.println("Starting server..");
-    HttpServer serverCEP = HttpServer.create(new InetSocketAddress(portCEP), 0);
     
-    serverCEP.createContext(context, new MyHandler());
-    serverCEP.start();
+    Properties prop = new Properties();
+    InputStream input = null;
     
-    System.out.println("The server is now listening on\nPort: " + portCEP + "\nContext: " + context + "\n");
+    try{
+	    input = new FileInputStream("config.properties");
+	   
+	    prop.load(input);
+	    portCEP = Integer.parseInt(prop.getProperty("MONITOR_PORT"));
+	    GCM_API_KEY = prop.getProperty("GCM_API_KEY");
+	    GCM_DEVICE_ID = prop.getProperty("GCM_DEVICE_ID");
+	    context = prop.getProperty("MONITOR_CONTEXT");
+	    om2mIp = prop.getProperty("OM2M_IP");
+	    postToGcmBool = Boolean.parseBoolean(prop.getProperty("POST_TO_GCM"));
+	    GCM_TITLE = prop.getProperty("GCM_TITLE");
+	    om2mPathToProduct = prop.getProperty("OM2M_PATH_TO_PRODUCT");
+	    om2mDeviceName = prop.getProperty("OM2M_DEVICE_NAME"); 
+	    om2mContainerName = prop.getProperty("OM2M_CONTAINER_NAME"); 
+	    autoSubscribe = Boolean.parseBoolean(prop.getProperty("MONITOR_AUTO_SUBSCRIBE"));
+	    om2mUsername = prop.getProperty("OM2M_USERNAME");
+	    om2mPassword = prop.getProperty("OM2M_PASSWORD");
+	    om2mSubContainer = prop.getProperty("OM2M_SUB_CONTAINER");
+	    
+	    
+	    HttpServer serverCEP = HttpServer.create(new InetSocketAddress(portCEP), 0);
+	    
+	    serverCEP.createContext(context, new MyHandler());
+	    serverCEP.start();
+	    
+	    System.out.println("The server is now listening on port: " + portCEP + ", context: " + context + "\n");
+	    
+	    if(autoSubscribe){
+	    	subscribeToOm2mContainer();	    
+	    }
+	    
+    } catch (Exception ex) {
+		ex.printStackTrace();
+		
+	} finally {
+		if (input != null) {
+			try {
+				input.close();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+    
   }
+  
+  public static void subscribeToOm2mContainer(){
+  	//SUBSCRIBE
+	    String rawData = "<m2m:sub xmlns:m2m='http://www.onem2m.org/xml/protocols'>" +
+						    "<nu>http://" + om2mIp + ":" + portCEP + context + "</nu>"+
+						    "<nct>2</nct>" +
+						 "</m2m:sub>";
+	    
+	    HttpURLConnection conn = null;
+	    
+	    try {
+	    	URL url = new URL("http://" + om2mIp + ":8080/~"+om2mPathToProduct+"/"+om2mDeviceName+"/"+om2mContainerName);
+		    conn = (HttpURLConnection) url.openConnection();
+	    	
+	        conn.setReadTimeout(10000);
+	        conn.setConnectTimeout(15000);
+	        conn.setRequestMethod("POST");
+	        conn.setDoInput(true);
+	        conn.setDoOutput(true);
+	        conn.setRequestProperty("X-M2M-Origin", om2mUsername + ":" + om2mPassword);
+	        conn.setRequestProperty("X-M2M-NM", om2mSubContainer);
+	        conn.setRequestProperty("Content-Type", "application/xml;ty=23");
+	        String body = rawData;
+	        OutputStream output = new BufferedOutputStream(conn.getOutputStream());
+	        output.write(body.getBytes());
+	        output.flush();
+	        
+	        int responseCode = conn.getResponseCode();
+	        String responseContent = conn.getContent().toString();
+	        	        
+	        if(responseCode == 201){
+	        	System.out.println("Monitor is subscribed to " + "http://" + om2mIp + ":8080/~"+om2mPathToProduct+"/"+om2mDeviceName+"/"+om2mContainerName);
+	        }
+	        
+	    } catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		
+	    } finally {
+	    	if(conn != null){
+	    		conn.disconnect();
+	    	}
+	    }
+  }
+  
 
  static class MyHandler implements HttpHandler
   {
@@ -80,7 +184,9 @@ public class Monitor
 		        		        
 		        System.out.println(value);
 		        
-		        //postToGCM("Alert", value);
+		        if(postToGcmBool){
+		        	postToGCM(GCM_TITLE, value);
+		        }
 		      
     	 }
      
@@ -92,7 +198,7 @@ public class Monitor
       
       
     }
-
+    
      public static void postToGCM(String title, String message){
 
      	try{
